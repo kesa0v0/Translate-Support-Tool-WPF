2b6d075f7b532c65331f39e637cd2412f422014f
kesa <kesa0v0@gmail.com>
2020-08-26 22:51:54 +0900

    init

---
+++ Translate-Support-Tool-WPF-Main/App.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Translate_Support_Tool_WPF_Main
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+    }
+}
---
+++ Translate-Support-Tool-WPF-Main/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace Translate_Support_Tool_WPF_Main
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
