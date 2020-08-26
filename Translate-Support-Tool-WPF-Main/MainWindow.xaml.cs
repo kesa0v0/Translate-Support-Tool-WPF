bea511ccad120a172aa2a8449668d467c78c472c
kesa <kesa0v0@gmail.com>
2020-08-27 01:32:25 +0900

    adding filemanager class

--- Translate-Support-Tool-WPF-Main/MainWindow.xaml.cs
+++ Translate-Support-Tool-WPF-Main/MainWindow.xaml.cs
@@ -1,13 +1,42 @@
-﻿namespace Translate_Support_Tool_WPF_Main
+﻿using System;
+using System.Collections.Generic;
+
+namespace Translate_Support_Tool_WPF_Main
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow
     {
         public MainWindow()
         {
             InitializeComponent();
+
+            var fileManager = new FileManager();
+            
+            var rawFile = fileManager.ReadFile(@"C:\_Storage\Programming\MyProject\WPF\Translate-Support-Tool-WPF\Translate-Support-Tool-WPF-Main\testData\test.yml");
+            // var file = fileManager.Yaml(rawFile);
+
+            // Context.Content = file[0];
+            // Origin.Content = file[1];
+
+            Context.Content = rawFile[1].Split(':')[0];
+            Context.Content = rawFile[1].Split(':')[1].Substring(2, -1);
+        }
+    }
+
+    class FileManager
+    {
+        public string[] ReadFile(string location)
+        {
+             return System.IO.File.ReadAllLines(location);
+        }
+
+        public Dictionary<int, string[]> Yaml(string[] rawData)
+        {
+            var dictionary = new Dictionary<int, string[]>();
+            
+            // processing
+            // DataStructure = {0, ("origin", "dest")}
+            
+            return dictionary;
         }
     }
 }
