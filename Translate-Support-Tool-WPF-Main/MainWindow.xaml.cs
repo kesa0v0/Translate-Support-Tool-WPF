c43fb1bd4c3fd50624ef32e382ae82618544b05f
kesa <kesa0v0@gmail.com>
2020-08-30 21:36:43 +0900

    listview to list box?

--- Translate-Support-Tool-WPF-Main/MainWindow.xaml.cs
+++ Translate-Support-Tool-WPF-Main/MainWindow.xaml.cs
@@ -17,42 +17,19 @@ namespace Translate_Support_Tool_WPF_Main
             var rawFile = fileManager.ReadFile(@"C:\_Storage\Programming\MyProject\WPF\Translate-Support-Tool-WPF\Translate-Support-Tool-WPF-Main\testData\test.yml");
             var file = fileManager.Yaml(rawFile);
 
-            foreach (var t in rawFile)
-            {
-                ListViewData.GetInstance().Add(new ListViewData() { Context = t });
-            }
-            
-            TextList.ItemsSource = ListViewData.GetInstance();
-            
-            TextList.SelectionChanged
+            // TextList.SelectionChanged
         }
     }
-
-    class ListViewData
-    {
-        public string Context { get; set; }
-
-        private static List<ListViewData> instance;
-
-        public static List<ListViewData> GetInstance()
-        {
-            if (instance == null)
-                instance = new List<ListViewData>();
-
-            return instance;
-        }
 }
-
-    class FileManager
+class FileManager
+{
+    public string[] ReadFile(string location)
     {
-        public string[] ReadFile(string location)
-        {
-             return System.IO.File.ReadAllLines(location);
-        }
+         return System.IO.File.ReadAllLines(location);
+    }
 
-        public string Yaml(string[] rawData)
-        {
-            return "";
-        }
+    public string Yaml(string[] rawData)
+    {
+        return "";
     }
 }
