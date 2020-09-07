7ae8453ba01d2e3a4b94686341705dd0a0b40a08
kesa <kesa0v0@gmail.com>
2020-09-07 23:44:18 +0900

    add listbox check listener

--- Translate-Support-Tool-WPF-Main/MainWindow.xaml.cs
+++ Translate-Support-Tool-WPF-Main/MainWindow.xaml.cs
@@ -17,8 +17,27 @@ namespace Translate_Support_Tool_WPF_Main
             var rawFile = fileManager.ReadFile(@"C:\_Storage\Programming\MyProject\WPF\Translate-Support-Tool-WPF\Translate-Support-Tool-WPF-Main\testData\test.yml");
             var file = fileManager.Yaml(rawFile);
 
-            // TextList.SelectionChanged
+            List<TextListItem> items = new List<TextListItem>();
+            items.Add(new TextListItem() { Context = "context1", Origin = "test1"});
+            items.Add(new TextListItem() { Context = "context1", Origin = "test2"});
+            items.Add(new TextListItem() { Origin = "test3"});
+            TextList.ItemsSource = items;
         }
+
+        private void TextList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (TextList.SelectedItem != null)
+            {
+                Context.Content = (TextList.SelectedItem as TextListItem)?.Context;
+                Origin.Content = (TextList.SelectedItem as TextListItem)?.Origin;
+            }
+        }
+    }
+
+    public class TextListItem
+    {
+        public string Context { get; set; }
+        public string Origin { get; set; }
     }
 }
 class FileManager
