992a75905ea3cba15969dd636c47a591a023aa24
kesa <kesa0v0@gmail.com>
2020-10-09 02:39:39 +0900

    add WordDictionary to Manager

--- Translate-Support-Tool-WPF-Main/WordDictionaryManager.cs
+++ Translate-Support-Tool-WPF-Main/WordDictionaryManager.cs
@@ -1,7 +1,23 @@
-﻿namespace Translate_Support_Tool_WPF_Main
+﻿using System;
+using System.Collections.Generic;
+
+namespace Translate_Support_Tool_WPF_Main
 {
     public class WordDictionaryManager
     {
+        public WordDictionary WordDictionary = new WordDictionary();
         
+        
+    }
+
+    public class WordDictionary : Dictionary<string, WordItem>
+    {
+    }
+
+    public class WordItem
+    {
+        // name is english
+        public int count { get; set; }
+        public string korean { get; set; }
     }
 }
