e5b95d13accdf2b281e8e3e17f67252f18ba429b
kesa <kesa0v0@gmail.com>
2020-10-25 02:55:35 +0900

    temp

---
+++ Translate-Support-Tool-WPF-Main/IsTranslatedConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Translate_Support_Tool_WPF_Main
+{
+    public class IsTranslatedConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
